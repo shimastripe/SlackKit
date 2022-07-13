ea4ec9ec44734d61b1ee9852e254c56c62af4c7f
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2022-07-13 13:28:15 +0900

    Update example settings

--- Examples/Leaderboard/Package.swift
+++ Examples/Leaderboard/Package.swift
@@ -1,14 +1,17 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "Leaderboard",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v10), .tvOS(.v10)
+    ],
     products: [
         .executable(name: "Leaderboard", targets: ["Leaderboard"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pvzig/SlackKit", .upToNextMinor(from: "4.6.0"))
+        .package(url: "https://github.com/pvzig/SlackKit", .revision("87af97c0099e2383ca8e08e3f62052d725783ee6"))
     ],
     targets: [
         .target(name: "Leaderboard",
